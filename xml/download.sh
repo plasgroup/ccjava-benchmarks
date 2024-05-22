e38da1d95a94bb7ffa95ba5b60ce6d295128f908
Tomoharu Ugawa <ugawa.tomoharu@kochi-tech.ac.jp>
2024-05-22 23:26:54 +0900

    remove obsolete versions of xmly

--- xml/download.sh
+++
@@ -1,5 +0,0 @@
-mkdir data
-cd data
-curl -O http://aiweb.cs.washington.edu/research/projects/xmltk/xmldata/data/pir/psd7003.dtd
-curl -O http://aiweb.cs.washington.edu/research/projects/xmltk/xmldata/data/pir/psd7003.xml
-cd ..
--- xml3/compile.sh
+++
@@ -1,8 +0,0 @@
-if [ x"$JAVA_HOME" = x ]; then
-  JAVAC=javac
-else
-  JAVAC=$JAVA_HOME/bin/javac
-fi
-
-exec $JAVAC -d bin src/Main.java
-
